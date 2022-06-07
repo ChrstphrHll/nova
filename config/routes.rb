2ea96eb758b8b5668d994663aec82708e4a276d4
Christopher Hall II <christopher.s.hall@duke.edu>
2022-06-07 10:46:02 -0400

    users and pilot connections established

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "home#index"
   resources :sparks, only: [:index, :show] do
     resources :active_powers
   end
+  devise_for :users
+  
+  resources :pilots
 end
